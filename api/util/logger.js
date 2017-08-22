1c6ae1f6ecd980cb42ca2704836829807aabb562
afaayfab <afaayfab@gmail.com>
2017-08-22 10:56:36 +0200

    inicialización de rabit al empezar y cambio a rabbit amqp con promesas

--- api/util/logger.js
+++ api/util/logger.js
@@ -6,80 +6,94 @@ let fs = require('fs')
 let moment = require('moment')
 let config = require('../../config')
 require('winston-daily-rotate-file')
-var publisher = require('../../api/util/rabbit/publisher')
 
-let level
-let transports = []
-var configJson
-if (process.env.NODE_ENV === 'development') {
-  level = config.dev.errorLevel
-  configJson = config.dev
-} else if (process.env.NODE_ENV === 'production') {
-  level = config.build.errorLevel
-  configJson = config.build
-} else {
-  level = config.debug.errorLevel
-  configJson = config.debug
+// let Promise = require('bluebird')
+let publisher = require('../../api/util/rabbit/publisher')
+
+// module.exports = logger
+let logger
+
+module.exports = function () {
+  return {
+    initLoggerRabbit: function () {
+      return publisher.manageConnection().then(conn => {
+        return publisher.createExchange(conn, 'logs').then(channel => {
+          return new Promise(
+            function (resolve, reject) {
+              let level
+              let transports = []
+              var configJson
+              if (process.env.NODE_ENV === 'development') {
+                level = config.dev.errorLevel
+                configJson = config.dev
+              } else if (process.env.NODE_ENV === 'production') {
+                level = config.build.errorLevel
+                configJson = config.build
+              } else {
+                level = config.debug.errorLevel
+                configJson = config.debug
+              }
+
+              const logDirectory = path.join(configJson.filelog, '')
+              fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+              transports.push(new winston.transports.DailyRotateFile({
+                level: level,
+                colorize: true,
+                prettyPrint: true,
+                filename: logDirectory,
+                datePattern: 'yyyy-MM-dd.',
+                prepend: true,
+                json: true,
+                align: true,
+                humanReadableUnhandledException: true,
+                timestamp: () => { return moment(moment()).locale('es').format('DD/MM/YYYY HH:MM:ss') }
+
+              }))
+
+              transports.push(new winston.transports.Console({
+                level: level,
+                colorize: true,
+                prettyPrint: true,
+                align: true,
+                humanReadableUnhandledException: true,
+                timestamp: () => { return moment(moment()).locale('es').format('DD/MM/YYYY HH:MM:ss') }
+                // ,
+                // handleExceptions: process.env.NODE_ENV === "production"
+              }))
+
+              logger = new winston.Logger({
+                level: level,
+                transports: transports,
+                exitOnError: false,
+                levels: {
+                  'error': 0,
+                  'warn': 1,
+                  'info': 2,
+                  'verbose': 3,
+                  'debug': 4,
+                  'silly': 5
+                },
+                colors: {
+                  'error': '',
+                  'warn': 'yellow',
+                  'info': 'white',
+                  'verbose': 'blue',
+                  'debug': 'green',
+                  'silly': 'orange'
+                }
+              })
+              logger.on('logging', function (transport, level, msg, meta) {
+                publisher.publishInExchange(channel, 'logs', level, msg)
+              })
+              resolve(channel)
+            }
+          )
+        })
+      })
+    },
+    getLogger: function () {
+      return logger
+    }
+  }
 }
-publisher.managePublisher(configJson, 'logs')
-
-const logDirectory = path.join(configJson.filelog, '')
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-
-transports.push(new winston.transports.DailyRotateFile({
-  level: level,
-  colorize: true,
-  prettyPrint: true,
-  filename: logDirectory,
-  datePattern: 'yyyy-MM-dd.',
-  prepend: true,
-  json: true,
-  align: true,
-  humanReadableUnhandledException: true,
-  timestamp: () => { return moment(moment()).locale('es').format('DD/MM/YYYY HH:MM:ss') }
-
-}))
-
-transports.push(new winston.transports.Console({
-  level: level,
-  colorize: true,
-  prettyPrint: true,
-  align: true,
-  humanReadableUnhandledException: true,
-  timestamp: () => { return moment(moment()).locale('es').format('DD/MM/YYYY HH:MM:ss') }
-  // ,
-  // handleExceptions: process.env.NODE_ENV === "production"
-}))
-
-let logger = new winston.Logger({
-  level: level,
-  transports: transports,
-  exitOnError: false,
-  levels: {
-    'error': 0,
-    'warn': 1,
-    'info': 2,
-    'verbose': 3,
-    'debug': 4,
-    'silly': 5
-  },
-  colors: {
-    'error': 'red',
-    'warn': 'yellow',
-    'info': 'white',
-    'verbose': 'blue',
-    'debug': 'green',
-    'silly': 'orange'
-  }
-})
-
-logger.on('logging', function (transport, level, msg, meta) {
-  if (transport.name === 'console') {
-    publisher.publishInExchange(level, msg)
-  }
-
-//  publisher.publishInExchange(logExchange, level, msg)
-  // console.log(moment(moment()).locale('es').format('DD/MM/YYYY HH:MM:ss') + '->' + level + '->' + msg + '->meta' + meta)
-})
-
-module.exports = logger
--- api/util/rabbit/publisher.js
+++ api/util/rabbit/publisher.js
@@ -1,24 +1,49 @@
+/*
+let Promise = require('bluebird')
+let amqp = Promise.promisifyAll(require('amqp'))
 
-let amqp = require('amqp')
-
-var connection = amqp.createConnection({host: 'localhost'})
 var exchange
-exports.managePublisher = function managePublisher (configEnv, exchangeName) {
+exports.createExchange = function createExchange (connection, exchangeName) {
   /// Create a connection to your RabbitMQ  
-  connection.on('ready', function () {
+  return new Promise((resolve, reject) => {
     connection.exchange(exchangeName, {
       type: 'topic', /// This is the type of exchange I want to use
       confirm: false
     }, function (exh) {
       exchange = exh
+      resolve(exh)
     })
   })
+
+  // })
+}
+
+exports.manageConnection = function manageConnection () {
+  return amqp.createConnection({ host: 'localhost' })
 }
 
 exports.publishInExchange = function publishInExchange (routintKey, message) {
-  if (exchange !== undefined) {
-    exchange.publish(routintKey, message, {
-      durable: true
-    })
-  }
+  exchange.publish(routintKey, message, {
+    durable: true
+  })
 }
+*/
+
+var amqp = require('amqplib')
+exports.manageConnection = function manageConnection () {
+  return amqp.connect('amqp://localhost')
+}
+
+exports.createExchange = function exchange (connection, exchange) {
+  return connection.createChannel().then(ch => {
+    ch.assertExchange(exchange, 'topic', {durable: true})
+    return ch
+  }).catch(err => {
+    console.log('Error creating exchange: ' + err)
+  })
+}
+
+exports.publishInExchange = function publishInExchange (ch, exchange, routingKey, msg) {
+  ch.publish(exchange, routingKey, Buffer.from(msg))
+}
+// exports.pub
